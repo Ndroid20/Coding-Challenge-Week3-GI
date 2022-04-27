d674b60fcab0ecbe7ebab5e931fc603d0d8742e8
Anthony Romero-Jorge <AnthonyRomero@MacBook-Pro.local>
2022-04-27 16:26:27 -0400

    added MEDIUM challenge

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ console.log(todayExercise("running"));
 console.log(todayExercise("swimming"));
 
 //MEDIUM
+const cutPizzaSlices = 8;
+
+const sharePizza = (numofPeople) =>
+  `Each person gets ${cutPizzaSlices / numofPeople} slices of pizza`;
+
+console.log(sharePizza(2));
+console.log(sharePizza(3));
